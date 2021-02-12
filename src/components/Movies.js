b4b7dc83cadc320303fff0a0b95dc2c334185b9c
Rmiverson <Rmiverson99@gmail.com>
2021-02-12 10:17:21 -0800

    done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movies.map(movie => {
+          return (
+            <div>
+              <h3>{ movie.title }</h3>
+              <h3>{ movie.time }</h3>
+              <ul>
+                { movie.genres.map(genre => {
+                  return (
+                    <li>{genre}</li>
+                  )
+                }) }
+              </ul>
+            </div>
+          )
+        }) } 
     </div>
   );
 };
